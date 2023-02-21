ef60e7916ccbff19f8018f176c847b58e11e76d9
xavierllabres1 <xavierllabres1@paucasesnovescifp.cat>
2023-02-21 17:22:31 +0100

    Inserció a Firebase (3.1)

--- lib/providers/users_provider.dart
+++ lib/providers/users_provider.dart
@@ -1,15 +1,35 @@
 // Aquest seria el provider per interactuar amb la BBDD (a través de DBProvider)
 // i els Widgets
 
+import 'dart:convert';
+
 import 'package:exam_practic/providers/providers.dart';
 import 'package:flutter/material.dart';
 import '../models/models.dart';
+import 'package:http/http.dart' as http;
 
 class UsersProvider extends ChangeNotifier {
   //Variables
   List<User> usuaris = []; //////// MODIFICAR VARIABLES
   GlobalKey<FormState> formKey = GlobalKey<FormState>();
   late User tempUsuari; // Usuari temporal
+
+  //Firebase
+  final String _baseUrl =
+      "examenpmm2223-default-rtdb.europe-west1.firebasedatabase.app";
+
+  // Just s'implemena la creacio a firebase ()
+  createUserFirebase(User u) async {
+    final url = Uri.https(_baseUrl, 'users.json');
+    final response = await http.post(url, body: u.toJson());
+    final decodedData = json.decode(response.body);
+  }
+
+  // No s'ham iplementat la funció de esborrar per falta de temps
+  // Ja que no és igual que el crear, sino que s'ha de recuerar la key
+  // per despres borrar a partir de la key
+
+  // End firebase
 
   //Metodes
   //INSERT
@@ -25,6 +45,9 @@ class UsersProvider extends ChangeNotifier {
     print(id);
     //Afagir objecte a llista (si volem)
     usuaris.add(u);
+
+    // Duplicar a Firebase
+    createUserFirebase(u);
 
     //actualitzar provider?
     notifyListeners();
@@ -66,6 +89,9 @@ class UsersProvider extends ChangeNotifier {
   esborraPerId(int id) async {
     // Executar la consulta
     final res = await DBProvider.db.deleteUser(id);
+
+    // Esborrar de Firebase
+    // deleteUserFirebase(id);
 
     carregaUsuaris();
     //Notificar el repintat
